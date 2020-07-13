451725baaf435d3a1f8db8f16742f3c319cfa36b
tamcy <tamcy@users.noreply.github.com>
2020-07-13 23:44:27 +0800

    update content for v2.026 release

--- js/font-loader.js
+++ js/font-loader.js
@@ -4,7 +4,7 @@ function loadFontFromCDN(version) {
             '_build/chiron-sans-hk/' :
             'https://cdn.jsdelivr.net/gh/tamcy/chiron-sans-hk@v' + version + '/build/webfont/';
 
-    $.each(['ExtraLight', 'Light', 'Normal', 'Medium', 'Bold', 'Heavy'], function (_, weight) {
+    $.each(['ExtraLight', 'Light', 'Regular', 'Medium', 'Bold', 'Heavy'], function (_, weight) {
         $("<link>", {
             rel: "stylesheet",
             type: "text/css",
